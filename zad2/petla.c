c199366ba622e1bc71dcfdea27f0ceb83f72e6e6
Michal Zmuda <zmudamichal92@gmail.com>
2013-03-16 11:03:45 +0100

    zad2final

---
+++ zad2/petla.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    int k=1;
+    long long unsigned res=0;
+    while(k){
+	res = res+1;
+    }
+    res=0;
+    return res;
+}
---
+++ zad2/zero.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    int i=7;
+    int k=0;
+    int res = i/k;
+    return res;
+}
