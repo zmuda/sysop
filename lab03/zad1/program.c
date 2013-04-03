53635d9b39bfad949e3fa477d5e0568fdac398c9
Michał Żmuda <zbysz@kasztel.(none)>
2013-04-03 19:37:44 +0200

    zad2beta, zad3 init

--- lab03/zad1/program.c
+++ lab03/zad1/program.c
@@ -7,8 +7,96 @@ typedef struct s {
     byte *dane;
 } struktura;
 
+void pokazStrukture(struktura* s, int rozmiar){
+    printf("\n<%i>\t",s->klucz);
+    while(rozmiar--){
+        printf("%i ",s->dane[rozmiar]);
+    }
+}
+
 void sortuj(char* plikNazwa){
     printf("\nsortuj\n");
+
+    int size, count;
+	#ifndef SYS
+	FILE * fp = fopen(plikNazwa, "r+b");
+	#else
+	int fp = open(plikNazwa, O_RDWR);
+	#endif
+
+	#ifndef SYS
+	if(fp == NULL)
+	#else
+	if(fp == -1)
+	#endif
+	{
+		printf("Nie znaleziono pliku!\n");
+		exit(89);
+	}
+
+	#ifndef SYS
+	fread(&size, sizeof(int), 1, fp);
+	fread(&count, sizeof(int), 1, fp);
+	#else
+	read(fp, &size, sizeof(int));
+	read(fp, &count, sizeof(int));
+	#endif
+
+	printf("Rozmiar struktury: %d\tLiczebnosc struktury: %d\n", size, count);
+
+    struktura *a,*b;
+    a=(struktura*)malloc(sizeof(struktura));
+    b=(struktura*)malloc(sizeof(struktura));
+    a->dane=(byte*)malloc(sizeof(byte)*size);
+    b->dane=(byte*)malloc(sizeof(byte)*size);
+    int i,posortowane = 0;
+    while(!posortowane){
+        posortowane=1;
+        for(i=0;i<count-1;i++){
+            #ifndef SYS
+			fseek(fp, 2*sizeof(int) + i * (sizeof(int) + sizeof(char)*size), SEEK_SET);
+			fread(a, 1, sizeof(int), fp);
+			fread(a->dane, 1, sizeof(byte)*size, fp);
+			fread(b, 1, sizeof(int), fp);
+			fread(b->dane, 1, sizeof(byte)*size, fp);
+			#else
+			lseek(fp, 2*sizeof(int) + i * (sizeof(int) + sizeof(char)*size), SEEK_SET);
+			read(fp, a, sizeof(int));
+            read(fp,a->dane,sizeof(char)*size);
+			read(fp, b, sizeof(int));
+            read(fp,b->dane,sizeof(char)*size);
+			#endif
+			printf("\n\t<%i> vs <%i>",a->klucz,b->klucz);
+			pokazStrukture(a,size);
+			pokazStrukture(b,size);
+			if(a->klucz>b->klucz){
+                #ifndef SYS
+                fseek(fp, 2*sizeof(int) + i * (sizeof(int) + sizeof(char)*size), SEEK_SET);
+                fwrite(b, sizeof(int),1,fp);
+                fwrite(b->dane, sizeof(byte)*size, 1, fp);
+                fwrite(a, sizeof(int),1,fp);
+                fwrite(a->dane, sizeof(byte)*size, 1, fp);
+                #else
+                lseek(fp, 2*sizeof(int) + i * (sizeof(int) + sizeof(char)*size), SEEK_SET);
+                write(fp, b, sizeof(int));
+                write(fp, b->dane, sizeof(char)*size);
+                write(fp, a, sizeof(int));
+                write(fp, a->dane, sizeof(char)*size);
+                #endif
+                posortowane = 0;
+			}
+        }
+        printf("\n");
+    }
+    free(a->dane);
+    free(a);
+    free(b->dane);
+    free(b);
+    #ifndef SYS
+	fclose(fp);
+	#else
+	close(fp);
+	#endif
 }
 
 void generuj(unsigned rozmiar, unsigned ilosc, char* plikNazwa){
@@ -27,13 +115,14 @@ void generuj(unsigned rozmiar, unsigned ilosc, char* plikNazwa){
 		fprintf(stderr, "Nie moge otworzyc pliku: %s\n", plikNazwa);
 		exit(98);
 	}
-	//naglowek
+	printf("Rozmiar struktury: %d\tLiczebnosc struktury: %d\n", rozmiar, ilosc);
+    //naglowek
 	#ifndef SYS
-	fwrite(&ilosc, sizeof(int), 1, fp);
 	fwrite(&rozmiar, sizeof(int), 1, fp);
+	fwrite(&ilosc, sizeof(int), 1, fp);
 	#else
-	write(fp, &ilosc, sizeof(int));
 	write(fp, &rozmiar, sizeof(int));
+	write(fp, &ilosc, sizeof(int));
 	#endif
     //cialo funkcji
     srand(time(NULL));
@@ -60,9 +149,11 @@ void generuj(unsigned rozmiar, unsigned ilosc, char* plikNazwa){
             //printf("%c",s->dane[j]);
 		}
 		#ifndef SYS
-		fwrite(s, sizeof(int) + sizeof(char)*rozmiar, 1, fp);
+		fwrite(s, sizeof(int),1,fp);
+        fwrite(s->dane, sizeof(byte)*rozmiar, 1, fp);
 		#else
-		write(fp, s, sizeof(int) + sizeof(char)*rozmiar);
+		write(fp, s, sizeof(int));
+		write(fp, s->dane, sizeof(char)*rozmiar);
 		#endif
 	}
 	#ifndef SYS
@@ -88,8 +179,8 @@ int main(int argc, char **argv){
                 return 3;
             } else {
                 unsigned rozmiar = atoi(argv[2]);
-                unsigned ilosc = atoi(argv[2]);
-                if(!rozmiar) {
+                unsigned ilosc = atoi(argv[3]);
+                if(!rozmiar&&argv[2][0]!='0') {
                     printf("\nniepoprawne argumeny - rozmiar struktur\n");
                     return 11;
                 }
