943645614dee9e6143a449d5b069fdc5fedd019c
Michał Żmuda <zbysz@kasztel.(none)>
2013-04-06 10:07:15 +0200

    zad2final

--- lab03/zad2/prog.c
+++ lab03/zad2/prog.c
@@ -3,7 +3,7 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <string.h>
 
 unsigned long zlicz2(char* dirPath){
     DIR *dp;
@@ -13,45 +13,39 @@ unsigned long zlicz2(char* dirPath){
     unsigned long suma = 0;
     if (dp != NULL){
         while (ep = readdir (dp)){
+            char tmp[256];
             if(ep->d_type==DT_DIR){
-                //printf("%i %s\n",ep->d_reclen,ep->d_name);
                 if( strcmp(ep->d_name,"..") && strcmp(ep->d_name,".") ){
-                    char tmp[80];
                     strcpy(tmp,dirPath);
                     strcat(tmp,ep->d_name);
                     strcat(tmp,"/");
-                    //printf("%s\n",tmp);
                     suma+=zlicz2(tmp);
                 }
             } else {
-                char tmp[80];
                 strcpy(tmp,dirPath);
                 strcat(tmp,ep->d_name);
                 if(stat(tmp,&fileStat) < 0)
                     return 1;
-                printf("%iB\t%s\n",fileStat.st_size,tmp);
-                suma+=fileStat.st_size;
+                int przyrost =fileStat.st_size;
+                printf("%iB\t%s\n",przyrost,tmp);
+                suma+=przyrost;
             }
         }
-        (void) closedir (dp);
+        closedir (dp);
     }
     else
-        perror ("Couldn't open the directory");
+        printf("Couldn't open the directory\n");
     return suma;
 }
-
 
 int main (int argc, char **argv){
     char tmp[80];
     if(argc<2){
-        strcpy(tmp,"./");
+        realpath("./",tmp);
     } else {
-        strcpy(tmp,argv[1]);
-        strcat(tmp,"/");
-        // wielokrotne '/' nie zmienia sensu ale brak zadnego juz tak - dodajemy
+        realpath(argv[1],tmp);
     }
+    strcat(tmp,"/");//bedziemy szukac wewnarz katalogu
     printf("\n\t%luB\n",zlicz2(tmp));
-
-
     return 0;
 }
--- lab03/zad2/prog2.c
+++ lab03/zad2/prog2.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
 #include <ftw.h>
 #include <time.h>
-#include <unistd.h>
-#include <errno.h>
-
+#include <limits.h>
 time_t data;
 int mode;
 
@@ -23,54 +18,25 @@ time_t dataNormalna(int date[])
 	t.tm_year = date[2]-1900;
 	return mktime(&t);
 }
-
-char * get_path(char * p)
-{
-	char * path = (char *)malloc(sizeof(char) * 256);
-	int error_code = errno;
-	if(path == NULL)
-	{
-		fprintf(stderr, "malloc error\n");
-		exit(error_code);
-	}
-	// zmienie working dir
-	if(chdir(p) != 0)
-	{
-		error_code = errno;
-		fprintf(stderr, "chdir error\n");
-		exit(error_code);
-	}
-	if(!getcwd(path, 256))
-	{
-		error_code = errno;
-		fprintf(stderr, "getcwd error\n");
-		exit(error_code);
-	}
-	return path;
-
-}
-
 int onlyDateComparision(time_t a,time_t b){
     struct tm aa=*localtime(&a);
     struct tm bb=*localtime(&b);
     return aa.tm_year*12*31+aa.tm_mon*31+aa.tm_mday -bb.tm_year*12*31-bb.tm_mon*31-bb.tm_mday;
 }
 // to bedzie wywolywane przez ftw
-int print(const char * filename, const struct stat *s, int flag)
-{
+int print(const char * filename, const struct stat *s, int flag){
 	if(!filename || !s)
 		return -1;
 	if(flag == FTW_F){
+	    char* res = (char *)malloc(sizeof(char) * 256);
         switch(mode){
-            case -1: if(onlyDateComparision(s->st_mtime,data)<0)printf("%s\n", filename);break;
-            case 0: if(onlyDateComparision(s->st_mtime,data)==0)printf("%s\n", filename);break;
-            case 1: if(onlyDateComparision(s->st_mtime,data)>0)printf("%s\n", filename);break;
-            default: exit(99);
+            case -1: if(onlyDateComparision(s->st_mtime,data)<0)printf("-> %s\n", realpath(filename,res));break;
+            case 0: if(onlyDateComparision(s->st_mtime,data)==0)printf("-> %s\n", realpath(filename,res));break;
+            case 1: if(onlyDateComparision(s->st_mtime,data)>0)printf("-> %s\n", realpath(filename,res));break;
+            default: exit(99);//nic nie piszemy, bo innego mode nie bedzie (porawnosc zapewniona)
         }
-
 	}
-
-	return 0;
+	return 0; //dla ftw mamy taki format, ze trzeba zwracac int'a - zero jezeli ok
 }
 
 int main(int argc, char ** argv)
@@ -89,7 +55,6 @@ int main(int argc, char ** argv)
         printf("wrong mode character; use eg. <\n\tused: %s\n",argv[2]);
         return 2;
 	}
-
 	int i = 0;
 	char * d = strtok(argv[3], " -:");
 	while(d && (i < 3))
@@ -98,9 +63,7 @@ int main(int argc, char ** argv)
 		// nastepne slowo
 		d = strtok(NULL, " -:");
 	}
-
 	data = dataNormalna(date);
 	ftw(argv[1], print, 1024);
-
 	return 0;
 }
