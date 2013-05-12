253679994d39c9e0d910a8caa5ef2d8fe1926675
Michał Żmuda <zbysz@kasztel.(none)>
2013-05-12 12:32:25 +0200

    ref12#finale

---
+++ lab05/zad3 (kopia)/zad3aSender.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <wait.h>
+#include <sys/types.h>
+int N,ctr=0;
+void handler2(int x){
+    int stat;
+    wait(&stat);
+    printf("\nKONIEC, odebrano %d z %d\n",ctr,N);
+    exit(0);
+}
+
+void handler1(int x){
+    printf("odebrano zwrotkę\n");
+    ctr++;
+}
+
+int main(int argc, char * argv[]) {
+    signal(SIGUSR1,handler1);
+    signal(SIGUSR2,handler2);
+    if (argc != 2 || (N = atoi(argv[1]))<1 ) {
+        printf("Nalezy podac ilosc sygnalow!\n");
+        //return 0;
+        N=2;
+    }
+    int i;
+    int PID=fork();
+    if(PID==0){
+        const char* name ="zad3aReciever";
+        if(execve(name,NULL,NULL) <0)printf("Blad exec\n");
+    }else{
+        sleep(1);
+        i=N;
+        while(i--){
+            printf("posłano SIGUSR1 #%d\n",N-i);
+            kill(PID,SIGUSR1);
+            sleep(1);
+        }
+        printf("posłano SIGUSR2\n");
+        kill(PID,SIGUSR2);
+
+        while(1){}
+    }
+    return 11;
+}
---
+++ lab05/zad3 (kopia)/zad3bReciever.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include <wait.h>
+
+
+int licznik = 0;
+
+void fun1(int x) {
+    printf(" -> Odbieram zadany sygnał...\n");
+    printf(" -> Zwracam zadany sygnał...\n");
+    kill(getppid(),SIGUSR1);
+    licznik++;
+}
+
+void fun2(int x) {
+    printf(" -> W sumie odebrano sygnalow: %d\n", licznik);
+    int parent = getppid();
+    /*
+    int l=licznik;
+    while(l--){
+        printf(" -> Zwracam zadany sygnał...\n");
+        kill(parent,SIGUSR1);
+        sleep(2);
+    }
+    */
+    printf(" -> Wysyłam potwierdzenie\n");
+    kill(parent,SIGUSR2);
+    exit(licznik);
+}
+
+int main() {
+    signal(SIGUSR1,fun1);
+    signal(SIGUSR2,fun2);
+    while(1){}
+    return -100;
+}
